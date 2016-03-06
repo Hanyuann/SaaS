aade8e657f38a7be8ed3b4dbdf9e73d1460ceadc
hanyuann <hanyuann@foxmail.com>
2016-03-06 05:49:36 +0000

    example!

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -40,19 +40,71 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+    return "Hello, "+name;
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+    if s == ''
+        return false
+    elsif s =~ /^[^a-zA-Z](.*)/
+        return false
+    elsif s =~ /^[aeiouAEIOU](.*)/
+        return false
+    else
+        return true
+    end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+    if s == ''
+        return false
+    elsif s =~ /^[01]*$/
+        if s.to_i(2) % "100".to_i(2) == 0
+            return true
+        else
+            return false
+        end
+    return false
+    end
 end
 
 # Part 3
 
 class BookInStock
     
+    @isbn = nil
+    @price = 0.00
+    
+    def initialize isbn,price
+       if isbn == '' || price <= 0
+           raise ArgumentError
+       end
+       @isbn,@price = isbn,price
+    end
+    
+    def isbn
+        @isbn
+    end
+    
+    def isbn= isbn
+        if isbn == ''
+            raise ArgumentError
+        end
+        @isbn = isbn
+    end
+    
+    def price
+        @price
+    end
+    
+    def price= price
+        if price <= 0
+            raise ArgumentError
+        end
+        @price = price
+    end
+    
+    def price_as_string
+        "$"+format("%0.2f",price)
+    end
 end
